678a4804aad72c70e3507ec6429bb10dfad4c8a2
Chris Quach <quachcw@uci.edu>
2023-01-08 17:48:25 -0800

    first push

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import json
 
+#open followers.json
 with open('followers.json') as file:
   followers_json = json.load(file)
 
+#open following.json
 with open('following.json') as file:
   following_json = json.load(file)
 
 followersLst = []
 followingLst = []
-# print(followers_json["relationships_followers"][0]['string_list_data'][0]['value'])
+
+#get each follower and add it to followersLst
 for followers in followers_json["relationships_followers"]:
   followersLst.append(followers['string_list_data'][0]['value'])
 
-# print(followersLst)
-
+#get each following and add it to followingLst
 for following in following_json["relationships_following"]:
   followingLst.append(following["string_list_data"][0]['value'])
-# print(followingLst)
 
+#list of people not following me back
 people_not_following_me_back = [i for i in followingLst if i not in followersLst]
+
+#print name of people not following me back
 for i, people in enumerate(people_not_following_me_back):
   print("{num}. {name}".format(num=i,name=people))
